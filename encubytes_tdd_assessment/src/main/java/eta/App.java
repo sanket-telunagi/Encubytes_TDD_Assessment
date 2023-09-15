8022b095f8a40c007f59059c8c100a613064c7c5
Pradnyavant <pradnyavantprogrammer@gmail.com>
2023-09-15 12:02:27 +0530

    project creation

---
+++ encubytes_tdd_assessment/src/main/java/eta/App.java
@@ -0,0 +1,11 @@
+package eta;
+// eta : Encubytes TDD Aseessment 
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
